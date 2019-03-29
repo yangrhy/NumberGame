c21b6602ef65386c5aa0073f3270fd9ab1400d9c
Ricky Yang <yangr@nsuok.edu>
2019-03-29 18:45:31 -0500

    Layout completed

--- app/src/main/java/com/example/numbergame/MainActivity.java
+++ app/src/main/java/com/example/numbergame/MainActivity.java
@@ -9,18 +9,21 @@ import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-    Button startButton;
+    Button processButton, randomButton, newNumButton;
     TextView instructions, oval;
     NumberPicker numPicker1, numPicker2, numPicker3, numPicker4
-                , randomNumPicker1, randomNumPicker2, randomNumPicker3, randomNumPicker4;
+                , randomNumPicker1, randomNumPicker2, randomNumPicker3, randomNumPicker4
+                , newNum1, newNum2, newNum3, newNum4;
     int num1, num2, num3, num4, sum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        instructions = (TextView)findViewById(R.id.textViewAnswer);
-        startButton = (Button)findViewById(R.id.randomButton);
+        instructions = (TextView)findViewById(R.id.enterNumLabel);
+        randomButton = (Button)findViewById(R.id.randomButton);
+        newNumButton = (Button)findViewById(R.id.newNumButton);
+        processButton = (Button)findViewById(R.id.processButton);
         numPicker1 = (NumberPicker)findViewById(R.id.numPicker1);
         numPicker2 = (NumberPicker)findViewById(R.id.numPicker2);
         numPicker3 = (NumberPicker)findViewById(R.id.numPicker3);
@@ -29,16 +32,20 @@ public class MainActivity extends AppCompatActivity {
         randomNumPicker2 = (NumberPicker)findViewById(R.id.randNumPicker2);
         randomNumPicker3 = (NumberPicker)findViewById(R.id.randNumPicker3);
         randomNumPicker4 = (NumberPicker)findViewById(R.id.randNumPicker4);
+        newNum1 = (NumberPicker)findViewById(R.id.newNum1);
+        newNum2 = (NumberPicker)findViewById(R.id.newNum2);
+        newNum3 = (NumberPicker)findViewById(R.id.newNum3);
+        newNum4 = (NumberPicker)findViewById(R.id.newNum4);
 
-        oval = (TextView)findViewById(R.id.ovalShape); // set a textView into activity_main.xml to use this
+        //oval = (TextView)findViewById(R.id.ovalShape); // set a textView into activity_main.xml to use this
 
-        numPicker1.setMinValue(1);
+        numPicker1.setMinValue(0);
         numPicker1.setMaxValue(9);
-        numPicker2.setMinValue(1);
+        numPicker2.setMinValue(0);
         numPicker2.setMaxValue(9);
-        numPicker3.setMinValue(1);
+        numPicker3.setMinValue(0);
         numPicker3.setMaxValue(9);
-        numPicker4.setMinValue(1);
+        numPicker4.setMinValue(0);
         numPicker4.setMaxValue(9);
 
 
@@ -47,6 +54,14 @@ public class MainActivity extends AppCompatActivity {
     private class ButtonClick implements Button.OnClickListener {
         public void onClick(View v) {
             if (v.getId() == R.id.randomButton) {
+                int[]numArray = new int[4];
+
+            }
+            else if (v.getId() == R.id.newNumButton) {
+                int[]numArray = new int[4];
+
+            }
+            else if (v.getId() == R.id.randomButton) {
                 int[]numArray = new int[4];
 
             }
