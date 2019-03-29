3357f62aad6e894f4764e92a8fb66efb9172ec07
Ricky Yang <yangr@nsuok.edu>
2019-03-29 16:17:29 -0500

    Added number pickers for randomized number and button for randomizing

--- app/src/main/java/com/example/numbergame/MainActivity.java
+++ app/src/main/java/com/example/numbergame/MainActivity.java
@@ -11,7 +11,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     Button startButton;
     TextView instructions, oval;
-    NumberPicker numPicker1, numPicker2, numPicker3, numPicker4;
+    NumberPicker numPicker1, numPicker2, numPicker3, numPicker4
+                , randomNumPicker1, randomNumPicker2, randomNumPicker3, randomNumPicker4;
     int num1, num2, num3, num4, sum;
 
     @Override
@@ -19,15 +20,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         instructions = (TextView)findViewById(R.id.textViewAnswer);
-        startButton = (Button)findViewById(R.id.startButton);
+        startButton = (Button)findViewById(R.id.randomButton);
         numPicker1 = (NumberPicker)findViewById(R.id.numPicker1);
         numPicker2 = (NumberPicker)findViewById(R.id.numPicker2);
         numPicker3 = (NumberPicker)findViewById(R.id.numPicker3);
         numPicker4 = (NumberPicker)findViewById(R.id.numPicker4);
+        randomNumPicker1 = (NumberPicker)findViewById(R.id.randNumPicker1);
+        randomNumPicker2 = (NumberPicker)findViewById(R.id.randNumPicker2);
+        randomNumPicker3 = (NumberPicker)findViewById(R.id.randNumPicker3);
+        randomNumPicker4 = (NumberPicker)findViewById(R.id.randNumPicker4);
+
         oval = (TextView)findViewById(R.id.ovalShape); // set a textView into activity_main.xml to use this
 
-        numPicker1.setMinValue(0);
+        numPicker1.setMinValue(1);
         numPicker1.setMaxValue(9);
+        numPicker2.setMinValue(1);
+        numPicker2.setMaxValue(9);
+        numPicker3.setMinValue(1);
+        numPicker3.setMaxValue(9);
+        numPicker4.setMinValue(1);
+        numPicker4.setMaxValue(9);
 
+
+    }
+
+    private class ButtonClick implements Button.OnClickListener {
+        public void onClick(View v) {
+            if (v.getId() == R.id.randomButton) {
+                int[]numArray = new int[4];
+
+            }
+        }
     }
 }
